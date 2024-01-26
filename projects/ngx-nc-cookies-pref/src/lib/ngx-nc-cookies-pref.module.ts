ee06b9974c4b2ec6f3a34c0e8e08bd603a7fa4b3
NicolaCocozza <nicola.cocozza@democom.it>
2024-01-26 17:56:49 +0100

    init library ngx-nc-cookies-pref

---
+++ projects/ngx-nc-cookies-pref/src/lib/ngx-nc-cookies-pref.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { NgxNcCookiesPrefComponent } from './ngx-nc-cookies-pref.component';
+
+
+
+@NgModule({
+  declarations: [
+    NgxNcCookiesPrefComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    NgxNcCookiesPrefComponent
+  ]
+})
+export class NgxNcCookiesPrefModule { }
---
+++ projects/ngx-nc-cookies-pref/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ngx-nc-cookies-pref
+ */
+
+export * from './lib/ngx-nc-cookies-pref.service';
+export * from './lib/ngx-nc-cookies-pref.component';
+export * from './lib/ngx-nc-cookies-pref.module';
